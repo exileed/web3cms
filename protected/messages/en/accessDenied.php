4230d18022e12dc5f388083ff592227d313e0693
phpdevmd <phpdevmd@777d9f84-3e64-11de-b48d-dbaac48c8fd3>
2010-04-16 18:19:18 +0000

    Add to RBAC actions 'delete' and 'ajaxDelete'.

--- protected/messages/en/accessDenied.php
+++ protected/messages/en/accessDenied.php
@@ -4,10 +4,14 @@
 $retval=array(
     'company/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse companies.',
     'companyPayment/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse company payments.',
+    'expense/delete' => 'Access denied. We are sorry, but you don\'t have enough rights to delete an expense record.',
+    'expense/deleteWhenInvoiceSet' => 'Access denied. We are sorry, but you don\'t have enough rights to delete an expense record associated with an invoice.',
     'expense/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse expenses.',
     'invoice/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse invoices.',
     'project/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse projects.',
     'task/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse tasks.',
+    'time/delete' => 'Access denied. We are sorry, but you don\'t have enough rights to delete a time record.',
+    'time/deleteWhenInvoiceSet' => 'Access denied. We are sorry, but you don\'t have enough rights to delete a time record associated with an invoice.',
     'time/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse the time records.',
     'user/grid' => 'Access denied. We are sorry, but you don\'t have enough rights to browse members.',
 );

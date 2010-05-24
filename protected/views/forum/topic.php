c863ebf59ba77cdfc57fdaa359712ad7a9549861
abdallah@gmx.com <abdallah@gmx.com@777d9f84-3e64-11de-b48d-dbaac48c8fd3>
2010-05-24 16:30:24 +0000

    reply to post function + replies counter

--- protected/views/forum/topic.php
+++ protected/views/forum/topic.php
@@ -1,10 +1,13 @@
 <?php
+$sectionName = (isset($section) ? $section->name : $models[0]->section->name);
+$sectionId = (isset($section) ? $section->id : $models[0]->section->id);
 $breadcrumbs = array(
-                'text'=>Yii::t('link', (isset($section) ? $section->name : $models[0]->section->name)),
+                'text'=>Yii::t('link', $sectionName),
+                'url'=>array('forum/section','sid'=>$sectionId),
                 'active'=>true,
     );
 echo $this->renderPartial('_common',array('breadcrumbs'=>$breadcrumbs));
-echo $this->renderPartial('_actionbar',array('id'=>$id));
+echo $this->renderPartial('_actionbar',array('tid'=>$tid,'sid'=>$models[0]->sectionId));
 ?>
 <?php if (!empty($models)):?>
 <table class="w3-grid-box ui-widget-content ui-corner-all" width="100%">
@@ -17,7 +20,6 @@ echo $this->renderPartial('_actionbar',array('id'=>$id));
     <tr class="w3-grid-row">
         <td valign="top" colspan="1" rowspan="2">
             <strong><?php echo CHtml::link(CHtml::encode($model->user->username), array('user/show', 'id'=>$model->user->id)); ?></strong><br />
-            
         </td>
         <td valign="top" align="right">
             <?php
@@ -32,5 +34,5 @@ echo $this->renderPartial('_actionbar',array('id'=>$id));
         <?php endforeach;?>
 </table>
 <br />
-<?php echo $this->renderPartial('_actionbar',array('id'=>$id));?>
+<?php echo $this->renderPartial('_actionbar',array('tid'=>$tid,'sid'=>$models[0]->sectionId));?>
 <?php endif;?>
